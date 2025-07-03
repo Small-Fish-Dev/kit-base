46c40eea22bc6492f2d9932dccb15857b4fcd347
yart <100040020+youarereadingthis@users.noreply.github.com>
2025-07-03 16:40:36 -0400

    Add some const tag and group strings to Entity

--- Project/game_fish/Libraries/kitbase/Code/Entity/Base/Entity.cs
+++ Project/game_fish/Libraries/kitbase/Code/Entity/Base/Entity.cs
@@ -1,9 +1,17 @@
 namespace GameFish;
 
 /// <summary>
-/// The most basic form of something that can separately exist.
+/// The most basic form of a physical object that can separately exist.
 /// </summary>
 public partial class Entity : Component, IEntity
 {
-    public const string FEATURE_ENTITY = "📦 Entity";
+	public const string FEATURE_ENTITY = "📦 Entity";
+	public const string FEATURE_NPC = "🤖 NPC";
+
+	public const string TAG_ENTITY = "entity";
+	public const string TAG_PROJECTILE = "projectile";
+
+	public const string TAG_ACTOR = "actor";
+	public const string TAG_PLAYER = "player";
+	public const string TAG_NPC = "npc";
 }
