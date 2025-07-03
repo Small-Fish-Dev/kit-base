19dbc504ef67a6c33afd9c39b4bb893f29e9e3f5
yart <100040020+youarereadingthis@users.noreply.github.com>
2025-07-03 16:52:19 -0400

    Add FloatRange util struct

---
+++ Project/game_fish/Libraries/kitbase/Code/Util/Structs/FloatRange.cs
@@ -0,0 +1,15 @@
+namespace GameFish;
+
+/// <summary>
+/// Min and max floats without a fixed value setting.
+/// </summary>
+public struct FloatRange
+{
+	[KeyProperty] public float Min { get; set; }
+	[KeyProperty] public float Max { get; set; }
+
+	public FloatRange() { }
+	public FloatRange( float min, float max ) { Min = min; Max = max; }
+
+	public static implicit operator FloatRange( RangedFloat r ) => new( r.Min, r.Max );
+}
