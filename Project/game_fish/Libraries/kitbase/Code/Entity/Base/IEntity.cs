a4f419eabb0b033efd7a35d0a2408353263993ca
yart <100040020+youarereadingthis@users.noreply.github.com>
2025-07-03 16:06:43 -0400

    IEntity cope summary

--- Project/game_fish/Libraries/kitbase/Code/Entity/Base/IEntity.cs
+++ Project/game_fish/Libraries/kitbase/Code/Entity/Base/IEntity.cs
@@ -1,5 +1,9 @@
 namespace GameFish;
 
+/// <summary>
+/// The most basic form of a physical object that can separately exist. <br />
+/// This is an interface so that non-components can be managed as an entity.
+/// </summary>
 public interface IEntity : ITransform, IHealth
 {
 }
