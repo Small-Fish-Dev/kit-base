cc0836de1dd4e44784b8064e4a3aa3b865d53e17
yart <100040020+youarereadingthis@users.noreply.github.com>
2025-07-05 15:07:20 -0400

    Component InEditor / InGame extension methods

---
+++ Project/game_fish/Libraries/kitbase/Code/Util/Extensions/InEditor.cs
@@ -0,0 +1,16 @@
+namespace GameFish;
+
+public static partial class GameFish
+{
+	/// <returns> If this component is valid and loaded in an editor scene(not in game/play mode). </returns>
+	public static bool InEditor( this Component c )
+	{
+		return c.IsValid() && (c.Scene?.IsEditor ?? true);
+	}
+
+	/// <returns> If this component is valid and loaded in a play mode scene(not scene/prefab editor). </returns>
+	public static bool InGame( this Component c )
+	{
+		return c.IsValid() && (c.Scene?.IsEditor ?? true);
+	}
+}
