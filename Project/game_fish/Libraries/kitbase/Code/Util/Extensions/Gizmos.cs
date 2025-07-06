bbea203880f91bbcd7912b8f2d1583ef665bf67e
yart <100040020+youarereadingthis@users.noreply.github.com>
2025-07-06 19:13:40 -0400

    Component.DrawArrow gizmo extension

--- Project/game_fish/Libraries/kitbase/Code/Util/Extensions/Gizmos.cs
+++ Project/game_fish/Libraries/kitbase/Code/Util/Extensions/Gizmos.cs
@@ -3,7 +3,54 @@ namespace GameFish;
 public static partial class GizmoExtensions
 {
 	/// <summary>
-	/// Draws a slightly fancy box that shows depth.
+	/// Draws a slightly fancy arrow that better indicates depth.
+	/// </summary>
+	/// <param name="comp"> The component drawing this. </param>
+	/// <param name="from"> The local origin of the arrow. </param>
+	/// <param name="to"> The local end point of the arrow. </param>
+	/// <param name="len"> The length of the arrow's head. </param>
+	/// <param name="w"> The width of the arrow's head. </param>
+	/// <param name="c"> The default color. </param>
+	/// <param name="tWorld"> The world transform to use. Defaults to the component's world transform. </param>
+	/// <returns> If the arrow could be drawn. </returns>
+	public static bool DrawArrow( this Component comp, Vector3 from, Vector3 to, Color c, float? len = null, float? w = null, Transform? tWorld = null )
+	{
+		if ( !comp.IsValid() )
+			return false;
+
+		using ( Gizmo.Scope( comp.Id + "-box" ) )
+		{
+			var t = tWorld ??= comp.WorldTransform;
+
+			Gizmo.Transform = t;
+
+			var arrowLen = len ?? 16f;
+			var arrowWidth = w ?? (arrowLen * 0.3f);
+
+			var isSelected = Gizmo.IsSelected;
+
+			// Depthless pass(see slightly through walls).
+			Gizmo.Draw.IgnoreDepth = true;
+			Gizmo.Draw.LineThickness = 1f;
+			Gizmo.Draw.Color = c.WithAlphaMultiplied( isSelected ? 0.2f : 0.1f );
+
+			// Gizmo.Draw.Line( from, to );
+			Gizmo.Draw.Arrow( from, to, arrowLen, arrowWidth );
+
+			// Depth pass(more directly visible).
+			Gizmo.Draw.IgnoreDepth = false;
+			Gizmo.Draw.LineThickness = isSelected ? 2f : 1f;
+			Gizmo.Draw.Color = c;
+
+			Gizmo.Draw.Line( from, to );
+			Gizmo.Draw.Arrow( from, to, arrowLen, arrowWidth );
+		}
+
+		return true;
+	}
+
+	/// <summary>
+	/// Draws a slightly fancy box that better indicates depth.
 	/// </summary>
 	/// <param name="comp"> The component drawing this. </param>
 	/// <param name="box"> The local bounds of the box. </param>
@@ -21,14 +68,14 @@ public static partial class GizmoExtensions
 
 			var isSelected = Gizmo.IsSelected;
 
-			// Draw a box underneath and through walls to better show depth.
+			// Depthless pass(see slightly through walls).
 			Gizmo.Draw.IgnoreDepth = true;
 			Gizmo.Draw.LineThickness = 1f;
 			Gizmo.Draw.Color = c.WithAlphaMultiplied( isSelected ? 0.2f : 0.1f );
 
 			Gizmo.Draw.LineBBox( box );
 
-			// Main box.
+			// Depth pass(more directly visible).
 			Gizmo.Draw.IgnoreDepth = false;
 			Gizmo.Draw.LineThickness = isSelected ? 2f : 1f;
 			Gizmo.Draw.Color = c;
@@ -40,14 +87,14 @@ public static partial class GizmoExtensions
 	}
 
 	/// <summary>
-	/// Draws a slightly fancy sphere that shows depth.
+	/// Draws a slightly fancy sphere that better indicates depth.
 	/// </summary>
 	/// <param name="comp"> The component drawing this. </param>
 	/// <param name="radius"> The radius of the sphere. </param>
 	/// <param name="center"> The offset of the sphere from world transform. </param>
 	/// <param name="c"> The default color. </param>
 	/// <param name="tWorld"> The world transform to use. Defaults to the component's world transform. </param>
-	/// <returns> If the box could be drawn. </returns>
+	/// <returns> If the sphere could be drawn. </returns>
 	public static bool DrawSphere( this Component comp, in float radius, in Vector3 center, Color c, Transform? tWorld = null )
 	{
 		if ( !comp.IsValid() || radius == 0f )
@@ -59,12 +106,14 @@ public static partial class GizmoExtensions
 
 			var isSelected = Gizmo.IsSelected;
 
+			// Depthless pass(see slightly through walls).
 			Gizmo.Draw.IgnoreDepth = true;
 			Gizmo.Draw.LineThickness = 1f;
 			Gizmo.Draw.Color = c.WithAlphaMultiplied( isSelected ? 0.2f : 0.1f );
 
 			Gizmo.Draw.LineSphere( center, radius );
 
+			// Depth pass(more directly visible).
 			Gizmo.Draw.IgnoreDepth = false;
 			Gizmo.Draw.LineThickness = isSelected ? 2f : 1f;
 			Gizmo.Draw.Color = c;
