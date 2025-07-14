2c5234a47d1bb5c8b1d41930870e677e9c38b5c0
yart <100040020+youarereadingthis@users.noreply.github.com>
2025-07-14 16:17:23 -0400

    Component.InGame fixed, both check for valid scene

--- Project/game_fish/Libraries/kitbase/Code/Util/Extensions/InEditor.cs
+++ Project/game_fish/Libraries/kitbase/Code/Util/Extensions/InEditor.cs
@@ -5,12 +5,12 @@ public static partial class GameFish
 	/// <returns> If this component is valid and loaded in an editor scene(not in game/play mode). </returns>
 	public static bool InEditor( this Component c )
 	{
-		return c.IsValid() && (c.Scene?.IsEditor ?? true);
+		return c.IsValid() && c.Scene.IsValid() && c.Scene.IsEditor;
 	}
 
 	/// <returns> If this component is valid and loaded in a play mode scene(not scene/prefab editor). </returns>
 	public static bool InGame( this Component c )
 	{
-		return c.IsValid() && (c.Scene?.IsEditor ?? true);
+		return c.IsValid() && c.Scene.IsValid() && !c.Scene.IsEditor;
 	}
 }
