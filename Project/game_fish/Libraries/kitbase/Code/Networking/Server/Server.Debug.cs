1dcfdcb2861eefb8a6823befdb38b0e3019d61ca
yart <100040020+youarereadingthis@users.noreply.github.com>
2025-07-15 02:06:31 -0400

    Put pawn list shit in Agents > Debug tab/group

--- Project/game_fish/Libraries/kitbase/Code/Networking/Server/Server.Debug.cs
+++ Project/game_fish/Libraries/kitbase/Code/Networking/Server/Server.Debug.cs
@@ -2,9 +2,13 @@ namespace GameFish;
 
 partial class Server
 {
+	public const string GROUP_DEBUG = BaseEntity.DEBUG;
+
 	[Property, ReadOnly]
+	[Feature( Agent.FEATURE_AGENT ), Group( GROUP_DEBUG )]
 	public List<BasePawn> AllPawns => BasePawn.GetAll<BasePawn>().ToList();
 
 	[Property, ReadOnly]
+	[Feature( Agent.FEATURE_AGENT ), Group( GROUP_DEBUG )]
 	public List<BasePawn> ActivePawns => BasePawn.GetAllActive<BasePawn>().ToList();
 }
